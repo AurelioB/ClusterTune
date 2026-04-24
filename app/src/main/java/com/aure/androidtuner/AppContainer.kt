53e9aed6f4304c58d2882842a3e59c5022435d2a
Aurelio Bernal <aurelio@bernalr.com>
2026-04-24 11:47:39 -0600

    add settings storage to AppContainer

--- app/src/main/java/com/aure/androidtuner/AppContainer.kt
+++ app/src/main/java/com/aure/androidtuner/AppContainer.kt
@@ -5,11 +5,16 @@ import com.aure.androidtuner.data.BundledPresetProvider
 import com.aure.androidtuner.data.CpuPolicyDetector
 import com.aure.androidtuner.data.PerformanceRepository
 import com.aure.androidtuner.data.ProfileStorage
+import com.aure.androidtuner.data.SettingsStorage
 import com.aure.androidtuner.root.PerformanceCommandBuilder
 import com.aure.androidtuner.root.RootCommandRunner
 
 class AppContainer(context: Context) {
     private val appContext = context.applicationContext
+
+    val settingsStorage: SettingsStorage by lazy {
+        SettingsStorage(appContext)
+    }
 
     val repository: PerformanceRepository by lazy {
         PerformanceRepository(
