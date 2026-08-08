e791938429d0de744875a3d8afb17911dd327a9a
Aurelio Bernal <aurelio@bernalr.com>
2026-08-07 22:14:11 -0600

    chore(release): prepare 1.2.0-rc2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.aure.clustertune"
         minSdk = 31
         targetSdk = 34
-        versionCode = 309
-        versionName = "1.2.0-rc1"
+        versionCode = 310
+        versionName = "1.2.0-rc2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
